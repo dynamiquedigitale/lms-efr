a07a801a20bef3614b60557764614517e7dae785
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2024-07-19 12:29:50 +0200

    chore: ajout du type textarea au form-group

--- resources/js/components/app/FormGroup.vue
+++ resources/js/components/app/FormGroup.vue
@@ -8,7 +8,8 @@
         <app v-else-if="type === 'checkbox'" v-model="modelValue" v-bind="checkboxAttrs" />
         <app-address-input v-else-if="type === 'address'" v-model="modelValue" v-bind="addressAttrs" @address:selected="(selected) => emit('address:selected', selected)" />
         <app-date-input v-else-if="isTypeDate" v-model:value="modelValue" v-bind="dateAttrs" />
-        <b-form-input v-else v-model="modelValue" v-bind="inputAttrs" />
+        <b-form-textarea v-else-if="type === 'textarea'" v-model="modelValue" v-bind="textareaAttrs" />
+		<b-form-input v-else v-model="modelValue" v-bind="inputAttrs" />
 
         <b-form-invalid-feedback v-if="error != null" class="d-inline my-1">{{ error }}</b-form-invalid-feedback>
     </b-form-group>
@@ -25,6 +26,7 @@ const modelValue = defineModel({ type: String })
 const emit = defineEmits(['update:modelValue', 'address:selected'])
 
 const props = defineProps({
+    autofocus: { default: false, type: Boolean },
     description: { default: '', type: String },
     disabled: { default: false, type: Boolean },
     error: { default: null, type: [String, null] },
@@ -33,21 +35,26 @@ const props = defineProps({
     label: { required: true, type: String },
     labelClass: { default: '', type: String },
     list: { default: null, type: [String, null] },
+    maxRows: { default: 6, type: Number },
+    noResize: { default: false, type: Boolean },
     options: { default: () => [], type: Array },
     placeholder: { default: '', type: String },
     readonly: { default: false, type: Boolean },
     reducer: { default: null, type: [Function, null] },
     required: { default: false, type: Boolean },
+    rows: { default: 2, type: Number },
     selectKey: { default: 'label', type: String },
     selectMultiple: { default: false, type: Boolean },
     selector: { default: 'value', type: String },
+    size: { default: 'md', type: String },
     state: { default: null, type: [Boolean, null] },
     type: { default: 'text', type: String },
     validFeedback: { default: null, type: [String, null] },
 })
 
 const inputAttrs = computed(() => ({
-    class: { 'border-danger': error.value != null },
+    autofocus: props.autofocus,
+	class: { 'border-danger': error.value != null },
     disabled: props.disabled,
     id: id.value,
     list: props.list,
@@ -95,10 +102,23 @@ const selectAttrs = computed(() => ({
     multiple: props.selectMultiple,
     options: props.options,
     placeholder: props.placeholder,
-    // required: props.required,
     reducer: props.reducer,
+    required: props.required,
     selector: props.selector,
     state: props.state,
+}))
+const textareaAttrs = computed(() => ({
+	autofocus: props.autofocus,
+    class: { 'border-danger': error.value != null },
+    disabled: props.disabled,
+    id: id.value,
+    maxRows: props.maxRows,
+	noResize: props.noResize,
+    placeholder: props.placeholder,
+    readonly: props.readonly,
+    required: props.required,
+	rows: props.rows,
+	size: props.size,
 }))
 const formGroupAttrs = computed(() => ({
     description: props.description,
