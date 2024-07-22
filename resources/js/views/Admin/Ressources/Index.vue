56065b4d1421b4c51f77551e9dc2cb94aac69409
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2024-07-22 11:13:42 +0200

    patch: recherche de ressource

--- resources/js/views/Admin/Ressources/Index.vue
+++ resources/js/views/Admin/Ressources/Index.vue
@@ -51,70 +51,7 @@
 										<span class="input-group-text border-0">
 											<app-icon name="search" />
 										</span>
-										<b-form-input class="files-filter border-0 bg-transparent" :placeholder="$t('rechercher')" />
-									</div>
-								</div>
-								<div class="d-flex align-items-center">
-									<div class="file-actions">
-										<i
-data-feather="arrow-down-circle"
-										  class="font-medium-2 cursor-pointer d-sm-inline-block d-none me-50"></i>
-										<i data-feather="trash" class="font-medium-2 cursor-pointer d-sm-inline-block d-none me-50"></i>
-										<i
-data-feather="alert-circle" class="font-medium-2 cursor-pointer d-sm-inline-block d-none"
-										  data-bs-toggle="modal" data-bs-target="#app-file-manager-info-sidebar"></i>
-										<div class="dropdown d-inline-block">
-										  <i class="font-medium-2 cursor-pointer" data-feather="more-vertical" role="button" id="fileActions" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-										  </i>
-										  <div class="dropdown-menu dropdown-menu-end" aria-labelledby="fileActions">
-											<a class="dropdown-item" href="#">
-											  <i data-feather="move" class="cursor-pointer me-50"></i>
-											  <span class="align-middle">Open with</span>
-											</a>
-											<a
-class="dropdown-item d-sm-none d-block" href="#" data-bs-toggle="modal"
-											  data-bs-target="#app-file-manager-info-sidebar">
-											  <i data-feather="alert-circle" class="cursor-pointer me-50"></i>
-											  <span class="align-middle">More Options</span>
-											</a>
-											<a class="dropdown-item d-sm-none d-block" href="#">
-											  <i data-feather="trash" class="cursor-pointer me-50"></i>
-											  <span class="align-middle">Delete</span>
-											</a>
-											<div class="dropdown-divider"></div>
-											<a class="dropdown-item" href="#">
-											  <i data-feather="plus" class="cursor-pointer me-50"></i>
-											  <span class="align-middle">Add shortcut</span>
-											</a>
-											<a class="dropdown-item" href="#">
-											  <i data-feather="folder-plus" class="cursor-pointer me-50"></i>
-											  <span class="align-middle">Move to</span>
-											</a>
-											<a class="dropdown-item" href="#">
-											  <i data-feather="star" class="cursor-pointer me-50"></i>
-											  <span class="align-middle">Add to starred</span>
-											</a>
-											<a class="dropdown-item" href="#">
-											  <i data-feather="droplet" class="cursor-pointer me-50"></i>
-											  <span class="align-middle">Change color</span>
-											</a>
-											<div class="dropdown-divider"></div>
-											<a class="dropdown-item" href="#">
-											  <i data-feather="download" class="cursor-pointer me-50"></i>
-											  <span class="align-middle">Download</span>
-											</a>
-										  </div>
-										</div>
-									  </div>
-									<div class="btn-group view-toggle ms-50" role="group">
-										<input type="radio" class="btn-check" name="view-btn-radio" data-view="grid" id="gridView" checked autocomplete="off" />
-										<label class="btn btn-outline-primary p-50 btn-sm" for="gridView">
-											<app-icon name="grid" />
-										</label>
-										<input type="radio" class="btn-check" name="view-btn-radio" data-view="list" id="listView" autocomplete="off" />
-										<label class="btn btn-outline-primary p-50 btn-sm" for="listView">
-											<app-icon name="list" />
-										</label>
+										<b-form-input v-model="filter.search" class="files-filter border-0 bg-transparent" :placeholder="$t('rechercher')" />
 									</div>
 								</div>
 							</div>
@@ -184,11 +121,10 @@ class="dropdown-item d-sm-none d-block" href="#" data-bs-toggle="modal"
 								</div>
 							</div>
 						</div>
-	  
-				</div>
-			  </div>
+					</div>
+			  	</div>
 			</div>
-		  </div>
+		</div>
 	</page-wrapper>
 	
 	
@@ -299,6 +235,8 @@ const filter = reactive({
 
 const items = computed(() => props.ressources.filter(({ type }) => {
 	return filter.type === 'all' ? true : filter.type === `${type}s`
+}).filter(({ nom }) => {
+	return filter.search === '' ? true: nom.toLowerCase().includes(filter.search.toLocaleLowerCase())
 }))
 
 const mediaTypes = computed(() => [ 
